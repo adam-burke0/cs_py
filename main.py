3a4e992737ca0c4114df5bcf3ec5252e0249f240
Adam Burke <adamburkegit@gmail.com>
2021-04-25 20:42:59 +0100

    added profile tracking

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import json
 with open("skins.txt", "r") as f_read, open("prices.txt", "w") as f_write:
   for line in f_read:
     skin_name = line.strip()
-    skin_info = requests.get("http://steamcommunity.com/market/priceoverview/?appid=730&currency=3&market_hash_name=" + skin_name)
+    skin_info = requests.get("http://csgobackpack.net/api/GetItemPrice/?id=" + skin_name)
+    print(skin_info)
     skin_json = json.loads(skin_info.text)
     median = skin_json["median_price"]
     f_write.write("\n" + skin_name + ": " + median.replace('€', '') )
